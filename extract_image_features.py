96f966001e3ee269ebb9406572fcf5645bfc1495
hai482k1 <hai482k1@gmail.com>
2025-12-24 20:11:45 +0700

    up

--- extract_image_features.py
+++ extract_image_features.py
@@ -6,21 +6,22 @@ from PIL import Image
 from tqdm import tqdm
 
 IMAGE_DIR = "/kaggle/input/imagenet-mini-1000-torch-mobilenet"
+
 OUTPUT_CSV = "image_features.csv"
-BATCH_SIZE = 1 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 model.eval()
 
-image_paths = []
-for root, _, files in os.walk(IMAGE_DIR):
+paths = []
+for root, dirs, files in os.walk(IMAGE_DIR):
     for f in files:
-        image_paths.append(os.path.join(root, f))
+        paths.append(os.path.join(root, f))
+
 with open(OUTPUT_CSV, "w", newline="") as f:
     writer = csv.writer(f)
 
-    for path in tqdm(image_paths):
+    for path in tqdm(paths):
         try:
             image = Image.open(path).convert("RGB")
         except:
@@ -29,10 +30,11 @@ with open(OUTPUT_CSV, "w", newline="") as f:
         image_input = preprocess(image).unsqueeze(0).to(device)
 
         with torch.no_grad():
-            image_features = model.encode_image(image_input)
-            image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+            feat = model.encode_image(image_input)
+            feat = feat / feat.norm(dim=-1, keepdim=True)
+
         img_name = os.path.basename(path)
 
-        writer.writerow(
-            [img_name] + image_features.cpu().numpy().flatten().tolist()
-        )
+        writer.writerow([img_name] + feat.cpu().numpy().flatten().tolist())
+
+print(f"🎉 Done! Saved to {OUTPUT_CSV}")
